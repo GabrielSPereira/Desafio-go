f4c6db249e667913a6b9723f74b54176efd8e9da
Gabriel Pereira <gabrielspereira2604@gmail.com>
2024-03-26 13:41:08 -0300

    Ajusta create table/inserts

--- Teste 2/queries.sql
+++ Teste 2/queries.sql
@@ -1,44 +1,44 @@
--- create
-CREATE TABLE HorariosTrabalho  (
-  CadastroId INTEGER NOT NULL,
-  DiaId  INTEGER NOT NULL,
-  Horas  INTEGER NOT NULL
-);
+CREATE TABLE DiasSemana
+(
+	DiaId		INT,
+	Descricao	VARCHAR(200)
+)
 
-CREATE TABLE DiasSemana  (
-  DiaId INTEGER  NOT NULL,
-  Descricao  TEXT NOT NULL
-);
+INSERT INTO DiasSemana
+SELECT 1, 'Domingo' UNION ALL
+SELECT 2, 'Segunda' UNION ALL
+SELECT 3, 'Terça'	UNION ALL
+SELECT 4, 'Quarta'	UNION ALL
+SELECT 5, 'Quinta'	UNION ALL
+SELECT 6, 'Sexta'	UNION ALL
+SELECT 7, 'Sábado'
 
--- insert
-INSERT INTO HorariosTrabalho VALUES (1, 2, 8);
-INSERT INTO HorariosTrabalho VALUES (1,	3, 8);
-INSERT INTO HorariosTrabalho VALUES (1, 5, 8);
-INSERT INTO HorariosTrabalho VALUES (1,	7, 1);
-INSERT INTO HorariosTrabalho VALUES (1, 7, 5);
-INSERT INTO HorariosTrabalho VALUES (2,	2, 4);
-INSERT INTO HorariosTrabalho VALUES (2,	3, 4);
-INSERT INTO HorariosTrabalho VALUES (2,	4, 4);
-INSERT INTO HorariosTrabalho VALUES (2,	5, 4);
-INSERT INTO HorariosTrabalho VALUES (2,	6, 4);
-INSERT INTO HorariosTrabalho VALUES (2,	7, 4);
-INSERT INTO HorariosTrabalho VALUES (3,	2, 6);
-INSERT INTO HorariosTrabalho VALUES (3,	3, 6);
-INSERT INTO HorariosTrabalho VALUES (3,	4, 6);
-INSERT INTO HorariosTrabalho VALUES (3,	5, 5);
-INSERT INTO HorariosTrabalho VALUES (3,	5, 6);
-INSERT INTO HorariosTrabalho VALUES (3,	6, 6);
+CREATE TABLE HorariosTrabalho 
+(
+	CadastroId	INT,
+	DiaId		INT,
+	Horas		INT
+)
 
-INSERT INTO DiasSemana VALUES (1, 	'Domingo');
-INSERT INTO DiasSemana VALUES (2, 	'Segunda');
-INSERT INTO DiasSemana VALUES (3, 	'Terça');
-INSERT INTO DiasSemana VALUES (4, 	'Quarta');
-INSERT INTO DiasSemana VALUES (5, 	'Quinta');
-INSERT INTO DiasSemana VALUES (6, 	'Sexta');
-INSERT INTO DiasSemana VALUES (7, 	'Sábado'); 
+INSERT INTO HorariosTrabalho
+SELECT 1,2,8 UNION ALL
+SELECT 1,3,8 UNION ALL
+SELECT 1,5,8 UNION ALL
+SELECT 1,7,1 UNION ALL
+SELECT 1,7,5 UNION ALL
+SELECT 2,2,4 UNION ALL
+SELECT 2,3,4 UNION ALL
+SELECT 2,4,4 UNION ALL
+SELECT 2,5,4 UNION ALL
+SELECT 2,6,4 UNION ALL
+SELECT 2,7,4 UNION ALL
+SELECT 3,2,6 UNION ALL
+SELECT 3,3,6 UNION ALL
+SELECT 3,4,6 UNION ALL
+SELECT 3,5,5 UNION ALL
+SELECT 3,5,6 UNION ALL
+SELECT 3,6,6
 
-
--- select 
 SELECT 
     dt.CadastroId,
     ds.Descricao AS DiaSemana,
@@ -50,6 +50,6 @@ CROSS JOIN
 LEFT JOIN 
     HorariosTrabalho ht ON ht.DiaId = ds.DiaId AND dt.CadastroId = ht.CadastroId
 GROUP BY 
-    dt.CadastroId, ds.Descricao
+    dt.CadastroId, ds.DiaId, ds.Descricao
 ORDER BY 
-    dt.CadastroId, FIELD(ds.Descricao, 'Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado');
+    dt.CadastroId, ds.DiaId;
